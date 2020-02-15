ba581f99c573f990bb166fa5b3d8c9e981111846
undefined4x <undefined4x@gmail.com>
2020-02-15 23:03:11 +0300

    emit

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 "use strict"
 
-Vue.component('vCircle', {
-    template : `<div class="circle" :style = "{background : $refs.colorController.color}"></div>
-                `,
-});
-
 Vue.component('vColorController', {
     template : `<div class="color_controller">
-                    <input type="text" v-model = "$parent.color">
+                    <input type="text" v-model.lazy = "color">
+                    <button @click = "$emit('lol', color)">push me</button>
                 </div>
                 `,
     data () {
@@ -17,6 +13,23 @@ Vue.component('vColorController', {
     },
 });
 
+Vue.component('vCircle', {
+    template : `<div class="circle" :style = "{background : col}"></div>
+                `,
+    props : {
+        col : {default : '#ccc', type : String }
+    },
+});
+
+
 let test = new Vue({
     el: '#vmApp',
+    data : {
+        color: '',
+    },
+    methods: {
+        getColor(val) {
+            this.color = '#' + val;
+        }
+    }
 });
